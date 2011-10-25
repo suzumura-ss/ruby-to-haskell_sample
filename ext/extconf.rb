b33ad8b12787bec1bc36caa2e2eca9fca54f1fd3
Toshiyuki Suumura <suz.labo@amail.plala.or.jp>
2011-10-26 04:44:33 +0900

    Bugfix: missing symbol: __gmp_set_memory_functions.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -4,17 +4,19 @@ require 'rbconfig'
 TARGET = 'factorial'
 C_SRC = %w{ main.c }
 HS_SRC = %w{ factorial.hs }
-HS_LIBS = %w{ base ghc-prim integer-gmp }
+HS_LIBS = %w{ base integer-gmp ghc-prim }
 
 CONF = RbConfig::MAKEFILE_CONFIG
 VALS = %w{prefix includedir RUBY_BASE_NAME ruby_version rubyhdrdir arch MKDIR_P INSTALL INSTALL_PROGRAM}.map{|k|
           "#{k}=#{CONF[k]}"
         }.join("\n")
-FLAGS = "FLAGS=-I$(rubyhdrdir) -I$(rubyhdrdir)/$(arch) -lHSrts -lffi -shared -dynamic -no-hs-main\n" \
-       + "FLAGS+=-no-auto-link-packages -I`ghc --print-libdir`/include -L`ghc --print-libdir`\n" \
+GHC_LIBDIR=`ghc --print-libdir`.strip
+FLAGS = "FLAGS=-I$(rubyhdrdir) -I$(rubyhdrdir)/$(arch) -shared -dynamic -no-hs-main -lHSrts -lffi\n" \
+       + "FLAGS+=-no-auto-link-packages -I#{GHC_LIBDIR}/include -L#{GHC_LIBDIR}\n" \
        + HS_LIBS.map{|l|
           'FLAGS+=' + `ghc-pkg describe #{l}|sed -ne 's/library-dirs: /-L/p;s/hs-libraries: /-l/p;'`.gsub(/\n/, ' ')
-         }.join("\n")
+         }.join("\n") \
+       + "\nFLAGS+=-lgmp\n"
 
 File.open("Makefile", "w") {|f|
   f.puts <<__MAKEFILE__

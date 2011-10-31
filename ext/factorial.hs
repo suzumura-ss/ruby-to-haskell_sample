8289c66592c090044dd77454c2ebd078fcddbaed
suzumura <suzumura@magika.(none)>
2011-11-01 04:44:42 +0900

    Type changed: CULong to CUInt.

--- ext/factorial.hs
+++ ext/factorial.hs
@@ -1,6 +1,6 @@
 module Factorial where
 import Foreign.C.Types
 
-foreign export ccall factorial :: CULong -> CULong
+foreign export ccall factorial :: CUInt -> CUInt
 factorial 0 = 1
 factorial x = x * factorial(x-1)
